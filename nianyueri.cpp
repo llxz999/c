788e4592a2ecdd30575327eff2758a39ba649053
llxz999 <55378411+llxz999@users.noreply.github.com>
2019-10-16 22:01:25 +0800

    年月日

---
+++ nianyueri.cpp
@@ -0,0 +1,15 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include "nianyueri.h"
+
+using namespace std;
+int main()
+{
+	time t;
+	initialtime(t);
+	Show(t);
+	return 0;
+}
